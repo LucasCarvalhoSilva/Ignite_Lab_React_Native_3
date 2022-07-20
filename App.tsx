1c3bc1a1c54896b8e07dacfc74de62c939f500aa
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2022-07-20 11:30:37 -0300

    atualizações segundo dia de curso

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import { SignIn } from './src/screens/SignIn'
-import { Loading } from './src/components/Loading'
+import { Routes } from './src/routes';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const[fontsLoaded] = useFonts( {Roboto_400Regular, Roboto_700Bold});
@@ -18,7 +18,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
